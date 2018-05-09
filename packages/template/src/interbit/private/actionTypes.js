8c1ec4f11393e414140eb6d577c31158875ed9a0
Jared Dykstra <jared-dykstra@users.noreply.github.com>
2018-05-09 16:04:36 -0600

    Ensures es5 under the `interbit` folder (#153)

--- packages/template/src/interbit/private/actionTypes.js
+++ packages/template/src/interbit/private/actionTypes.js
@@ -5,14 +5,4 @@ const actionTypes = {
   ADD: `${covenantName}/ADD`
 }
 
-// Export a proxy so an exception is thrown in case an undefined property is accessed
-module.exports = new Proxy(actionTypes, {
-  get: (obj, prop) => {
-    if (prop in obj) {
-      return obj[prop]
-    }
-    throw new Error(
-      `Invalid action type "${prop}" in covenant "${covenantName}"`
-    )
-  }
-})
+module.exports = actionTypes

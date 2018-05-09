09e449d61178b959074d9008fe7c963a7050201a
David Mills <30841719+djlmills@users.noreply.github.com>
2018-05-09 13:25:28 -0700

    Handle change to cli.connect interface

--- packages/interbit/src/chainManagement/connectToPeers.js
+++ packages/interbit/src/chainManagement/connectToPeers.js
@@ -1,16 +1,32 @@
-const connectToPeers = async (cli, peers = []) => {
+const connectToPeers = async (
+  cli,
+  peers = [],
+  maxRetries = 25,
+  timeout = 20000
+) => {
   console.log('CONNECTING TO PEERS', peers)
   for (const peer of peers) {
     const [toAddress, toPort] = peer.split(':')
-    await tryConnect({ cli, toAddress, toPort })
+    await tryConnect({ cli, toAddress, toPort, maxRetries, timeout })
   }
 }
 
-const tryConnect = async ({ cli, toAddress = 'localhost', toPort }) => {
+const tryConnect = async ({
+  cli,
+  toAddress = 'localhost',
+  toPort,
+  maxRetries = 25,
+  timeout = 20000
+}) => {
   if (toAddress && toPort) {
     try {
       console.log(`Connecting: ${toAddress}:${toPort}`)
-      await cli.connect(Number(toPort), toAddress)
+      await cli.connect(
+        Number(toPort),
+        toAddress,
+        Number(maxRetries),
+        Number(timeout)
+      )
       console.log(`Connected to: ${toAddress}:${toPort}`)
     } catch (error) {
       console.warn(`Connection failed: ${toAddress}:${toPort}: `, error)

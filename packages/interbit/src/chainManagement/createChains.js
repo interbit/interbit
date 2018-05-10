aa43fa575273d25102f97318a7f0d16a2352f264
David Mills <30841719+djlmills@users.noreply.github.com>
2018-05-09 17:49:10 -0700

    await for chain dispatch when starting

--- packages/interbit/src/chainManagement/createChains.js
+++ packages/interbit/src/chainManagement/createChains.js
@@ -46,7 +46,7 @@ const createChainsFromManifest = async (location, cli, manifest, options) => {
     }
 
     const chain = cli.getChain(chainId)
-    chain.dispatch({ type: '@@interbit/DEPLOY' })
+    await chain.dispatch({ type: '@@interbit/DEPLOY' })
   }
 }
 

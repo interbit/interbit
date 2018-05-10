c32bb5e32e6560918d1703355ed50957887db2be
David Mills <30841719+djlmills@users.noreply.github.com>
2018-05-10 13:07:16 -0700

    Update connect to peers to work with environment setting

--- packages/interbit/src/chainManagement/connectToPeers.js
+++ packages/interbit/src/chainManagement/connectToPeers.js
@@ -1,7 +1,10 @@
 // Port 443 appropriate for heroku deployment
 const DEFAULT_PORT = 443
 
-const connectToPeers = async (cli, peers = []) => {
+const connectToPeers = async (cli, manifestPeers = []) => {
+  const peerOverride = process.env.CONNECT_TO_PEERS || ''
+  const peers = peerOverride ? peerOverride.split(',') : manifestPeers
+
   console.log('CONNECTING TO PEERS', peers)
   for (const peer of peers) {
     const [toAddress, toPort] = peer.split(':')

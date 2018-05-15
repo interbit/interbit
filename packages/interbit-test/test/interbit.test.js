34be21ca472581f1e04107f257e74b20c4535d0f
David Mills <30841719+djlmills@users.noreply.github.com>
2018-05-15 10:05:42 -0700

    Add failing unit test for unsubscribe bug

--- packages/interbit-test/test/interbit.test.js
+++ packages/interbit-test/test/interbit.test.js
@@ -143,6 +143,31 @@ describe('interbit', () => {
 
           verifyApi(chain, expectedChainApi)
         })
+
+        // interbit-core 0.7.0 regression - unsubscribe() does not unsubscribe #186
+        it('subscribe and unsubscribe work', async () => {
+          const chainId = await cli.createChain()
+          const chain = await cli.getChain(chainId)
+          let unsubscribe = () => {}
+          let count = 0
+          console.time('Time to unsubscribe')
+          unsubscribe = chain.subscribe(() => {
+            console.log(`Subscribe callback: ${count}`)
+            count += 1
+            if (count === 1) {
+              unsubscribe()
+              console.timeEnd('Time to unsubscribe')
+            }
+          })
+          // Potentially brittle
+          // Test needs to wait for at least 2 blocks
+          // sleep timeout assumes a blocking frequency of 2 secs
+          // test timeout needs to be longer than the sleep period
+          await sleep(4500)
+          assert.equal(count, 1)
+        }).timeout(5000)
+
+        const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
       })
     })
   })

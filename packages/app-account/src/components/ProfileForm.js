6a30a5f944529c2c2265a089cea0856140f4facc
nicholeous <nicole.a.demers@gmail.com>
2018-05-22 16:45:23 -0700

    Improve mttf tests

--- packages/app-account/src/components/ProfileForm.js
+++ packages/app-account/src/components/ProfileForm.js
@@ -133,7 +133,7 @@ class ProfileForm extends Component {
 
     if (isEditable) {
       return (
-        <div>
+        <div id="ib-profile">
           {sectionIntro}
           {editForm}
         </div>
@@ -141,7 +141,7 @@ class ProfileForm extends Component {
     }
 
     return (
-      <div className="account-profile-form">
+      <div id="ib-profile" className="account-profile-form">
         {sectionIntro}
         <Button
           onClick={() => {
--- packages/interbit-test/test/platform/scripts/mttf.js
+++ packages/interbit-test/test/platform/scripts/mttf.js
@@ -3,12 +3,9 @@ const { Builder, By, until } = require('selenium-webdriver')
 const SITE = 'https://ib-dev-account.herokuapp.com/'
 const BROWSERSTACK_SERVER = 'http://hub-cloud.browserstack.com/wd/hub'
 
-// Input capabilities
 const capabilities = {
-  browserName: 'iPhone',
-  device: 'iPhone 7',
-  realMobile: 'true',
-  os_version: '10.3',
+  browserName: 'chrome',
+  version: '63',
   'browserstack.user': process.env.BROWSERSTACK_USER,
   'browserstack.key': process.env.BROWSERSTACK_KEY,
   'browserstack.console': 'errors',
@@ -23,14 +20,35 @@ const runBrowserStackTest = async () => {
 
   try {
     await driver.get(SITE)
-    await driver.wait(until.elementLocated(By.id('ib-create-account')), 10000)
+
+    await driver.wait(until.elementLocated(By.id('ib-create-account')), 20000)
     await driver.findElement(By.id('ib-create-account')).click()
     await driver.findElement(By.id('ib-github-create')).click()
     await driver.findElement(By.name('check')).click()
     await driver.findElement(By.name('continue')).click()
 
+    await driver.wait(until.elementIsEnabled(By.name('commit')), 20000)
+    await driver
+      .findElement(By.id('login_field'))
+      .sendKeys(process.env.GITHUB_USER)
+    await driver
+      .findElement(By.id('password'))
+      .sendKeys(process.env.GITHUB_PASS)
+    await driver.findElement(By.name('commit')).click()
+
+    // Only need to authorize if the user has never ever authed before
+    try {
+      await driver.wait(until.elementLocated(By.name('authorize')), 10000)
+      await driver.wait(until.elementIsEnabled(By.name('authorize')), 20000)
+      await driver.findElement(By.name('authorize')).click()
+    } catch (e) {
+      console.log('User was already authenticated')
+    }
+
+    await driver.wait(until.elementLocated(By.id('ib-signed-in')), 20000)
+
     await driver.getTitle().then(title => {
-      console.log(title)
+      console.log(`Successfully created account and loaded "${title}"`)
     })
   } finally {
     await driver.quit()

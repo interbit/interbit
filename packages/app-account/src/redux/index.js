55f5a2c2aa477a78d1a89f36f977431e187c8b68
David Mills <30841719+djlmills@users.noreply.github.com>
2018-05-25 17:40:22 -0700

    Simplify reducers

--- packages/app-account/src/redux/index.js
+++ packages/app-account/src/redux/index.js
@@ -1,9 +1,6 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
-import {
-  reducer as interbitReducer,
-  INTERBIT_REDUCER_KEY
-} from 'interbit-ui-tools'
+import { reducer as interbitReducer } from 'interbit-ui-tools'
 import exploreChainReducer from './exploreChainReducer'
 import { reducer as uiReducer } from './uiReducer'
 import { reducer as contentReducer } from './contentReducer'
@@ -12,6 +9,6 @@ export default combineReducers({
   form: formReducer,
   ui: uiReducer,
   content: contentReducer,
-  [INTERBIT_REDUCER_KEY]: interbitReducer,
+  interbit: interbitReducer,
   exploreChain: exploreChainReducer
 })

a8647fee05177c5a53d41a065d9c9c77f0da75db
Tyler Kellogg <tyler@btl.co>
2018-05-31 16:06:49 -0700

    Accounts MTTF Optimizer Example

--- packages/app-account/src/App.js
+++ packages/app-account/src/App.js
@@ -13,6 +13,49 @@ import paths from './constants/paths'
 import './config/amplitude'
 import './css/App.css'
 
+let firstButtonClicked = false
+let startAuthButtonClicked = false
+let continueButtonClicked = false
+
+function MTTFOptimizer({ isChainLoaded, isLoggedIn, state }) {
+  if (
+    isChainLoaded &&
+    !isLoggedIn &&
+    !window.location.pathname.startsWith('/account/oauth/gitHub')
+  ) {
+    if (['', '/'].includes(window.location.pathname) && !firstButtonClicked) {
+      const firstButton = document.getElementById('ib-test-create-account')
+      if (firstButton) {
+        console.log('Clicking first button')
+        firstButtonClicked = true
+        setTimeout(() => firstButton.click(), 300)
+      }
+    } else if (!startAuthButtonClicked) {
+      const startAuthButton = document.getElementById('ib-test-github-create')
+      if (startAuthButton) {
+        console.log('Clicking start auth')
+        startAuthButtonClicked = true
+        setTimeout(() => startAuthButton.click(), 300)
+      }
+    } else if (state.ui.modals.attention && !continueButtonClicked) {
+      const continueButton = document.getElementsByName('continue')[0]
+      if (continueButton) {
+        console.log('Clicking continue')
+        continueButtonClicked = true
+        setTimeout(() => continueButton.click(), 300)
+      }
+    }
+  } else if (
+    isChainLoaded &&
+    isLoggedIn &&
+    window.location.pathname.startsWith('/account/oauth/gitHub')
+  ) {
+    window.localStorage.clear()
+    window.indexedDB.deleteDatabase('IDBWrapper-interbit')
+    window.location = '/'
+  }
+}
+
 const mapStateToProps = state => {
   const chainAlias = CHAIN_ALIASES.PRIVATE
   const isChainLoaded = selectors.isChainLoaded(state, { chainAlias })
@@ -21,6 +64,9 @@ const mapStateToProps = state => {
     ? selectors.getChain(state, { chainAlias })
     : {}
   const isLoggedIn = isChainLoaded && !!chainState.profile['gitHub-identity']
+
+  MTTFOptimizer({ isChainLoaded, isLoggedIn, state })
+
   const gitHubUsername =
     isLoggedIn && chainState.profile['gitHub-identity'].login
 

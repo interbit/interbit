d85a34b465a8ccba0188d746aa19fda36ea9d25f
Tyler Kellogg <tyler@btl.co>
2018-06-01 16:55:47 -0700

    Cleanup logChains

--- packages/interbit/src/chainManagement/logChains.js
+++ packages/interbit/src/chainManagement/logChains.js
@@ -1,5 +1,7 @@
 const { loggingFactory } = require('../logging/logging-factory')
 
+const UPDATE_CHILDREN = '@@interbit/UPDATE_CHILDREN'
+
 const {
   getRootChildren,
   getChainIdByAlias
@@ -14,7 +16,19 @@ export const logChainsFromManifest = async ({ cli, manifest, logging }) => {
   }
 }
 
-const logChain = async ({ cli, chainAlias, chainId, format = 'console' }) => {
+const getChildrenIdsOfContentPayload = async ({ content }) =>
+  content.actions
+    .filter(a => a.type === UPDATE_CHILDREN)
+    .map(n => n.payload.createdChildIds)
+    .filter(String)
+
+const logChain = async ({
+  cli,
+  chainAlias,
+  chainId,
+  childLogging = false,
+  format = 'console'
+}) => {
   const chainInterface = cli.getChain(chainId)
   const logger = loggingFactory({
     format,
@@ -25,23 +39,17 @@ const logChain = async ({ cli, chainAlias, chainId, format = 'console' }) => {
 
   chainInterface.subscribe(() => {
     const appState = chainInterface.getState()
-    const blocks = appState.interbit.blocks
-    const content = blocks[blocks.length - 1].content
+    const { blocks } = appState.interbit
+    const { content } = blocks[blocks.length - 1]
+    const { errors } = content
     const logs = content.actions.map(a => a.type).asMutable()
-    if (Object.keys(content.errors).length) {
-      logs.push({ errors: content.errors })
-    }
+    if (Object.keys(errors).length) logs.push({ errors })
+
     logger.log(logs)
-    const updateChildren = content.actions.filter(
-      a => a.type === '@@interbit/UPDATE_CHILDREN'
-    )
-    if (updateChildren.length) {
-      const newChainIds = []
-        .concat(...updateChildren.map(n => n.payload.createdChildIds))
-        .filter(String)
-      newChainIds.forEach(async cId => {
-        logChain({ cli, chainAlias: 'child', chainId: cId, format })
-      })
-    }
+
+    if (childLogging)
+      getChildrenIdsOfContentPayload({ content }).forEach(childId =>
+        logChain({ cli, chainAlias: childId, chainId: childId, format })
+      )
   })
 }

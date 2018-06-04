564f2892e9e233fefc70e1cb1a2ef9dd3d9da38e
nicholeous <nicole.a.demers@gmail.com>
2018-06-04 10:32:08 -0700

    Refactor tests

--- packages/app-todo-list/src/tests/interbit/privateCovenant.test.js
+++ packages/app-todo-list/src/tests/interbit/privateCovenant.test.js
@@ -2,131 +2,108 @@ import assert from 'assert'
 import { reducer, initialState, actionCreators } from '../../interbit/private'
 
 describe('app-todo-list private covenant', () => {
-  it('ADD_TODO action', () => {
-    const validInputs = [
-      {
-        todo: { title: 'foo', description: 'bar' },
-        expectedTodos: [
-          { id: 0, title: 'foo', description: 'bar', completed: false }
-        ]
-      },
-      {
-        todo: { title: 'foo' },
-        expectedTodos: [
-          { id: 0, title: 'foo', description: undefined, completed: false }
-        ]
-      },
-      {
-        todo: { title: 'foo', description: 'bar' },
-        currentTodos: [
-          { id: 0, title: 'aaa', description: 'bbb', completed: false }
-        ],
-        expectedTodos: [
-          { id: 0, title: 'aaa', description: 'bbb', completed: false },
-          { id: 1, title: 'foo', description: 'bar', completed: false }
-        ]
-      }
-    ]
+  const aTodo = { title: 'foo', description: 'bar' }
+  const aCompletedTodo = { ...aTodo, completed: true, id: 0 }
+  const constructState = todos => initialState.set('todos', todos)
 
-    const invalidInputs = [
-      {
-        todo: {}
-      },
-      { todo: { description: 'foo' } }
-    ]
-
-    validInputs.forEach(testCase => {
-      const { todo, expectedTodos, currentTodos = [] } = testCase
-      const action = actionCreators.addTodo(todo.title, todo.description)
-      const startState = initialState.set('todos', currentTodos)
-      const newState = reducer(startState, action)
-      assert.deepEqual(newState.todos, expectedTodos)
-    })
-
-    invalidInputs.forEach(testCase => {
-      const { todo } = testCase
-      const action = actionCreators.addTodo(todo.title, todo.description)
-      const startState = initialState.set('todos', [])
-      const newState = reducer(startState, action)
-      assert.deepEqual(newState.todos, startState.todos)
-    })
-  })
-
-  it('EDIT_TODO action', () => {
-    const currentTodos = [
-      { id: 0, title: 'cat', description: 'woof', completed: false }
-    ]
-
-    const validInputs = [
-      { id: 0, title: 'cat', description: 'meow', completed: false },
-      { id: 0, title: 'cat', description: undefined, completed: false },
-      { id: 0, title: 'cat', description: '', completed: false },
-      { id: 0, title: 'dog', description: 'meow', completed: false }
-    ]
-
-    const invalidInputs = [{ todo: {} }, { todo: { id: 0 } }]
-
-    validInputs.forEach(testCase => {
-      const { id, title, description, completed } = testCase
-      const action = actionCreators.editTodo(id, title, description, completed)
-      const startState = initialState.set('todos', currentTodos)
-      const newState = reducer(startState, action)
-      assert.deepEqual(newState.todos, [{ id, title, description, completed }])
-    })
-
-    invalidInputs.forEach(testCase => {
-      const { todo } = testCase
-      const action = actionCreators.editTodo(
-        todo.id,
-        todo.title,
-        todo.description,
-        todo.completed
-      )
-      const startState = initialState.set('todos', currentTodos)
-      const newState = reducer(startState, action)
-      assert.deepEqual(newState.todos, startState.todos)
-    })
-  })
-
-  it('TOGGLE_TODO action', () => {
-    const validInputs = [
-      {
-        id: 0,
-        currentTodos: [
-          { id: 0, title: 'foo', description: 'bar', completed: false }
-        ],
-        expectedTodos: [
-          { id: 0, title: 'foo', description: 'bar', completed: true }
-        ]
-      }
-    ]
-
-    const invalidInputs = [
-      {},
-      { id: undefined },
-      { id: 'foo' },
-      { id: Number.NaN },
-      { id: Number.POSITIVE_INFINITY },
-      { id: Number.NEGATIVE_INFINITY }
-    ]
-
-    validInputs.forEach(testCase => {
-      const { id, currentTodos, expectedTodos } = testCase
-      const action = actionCreators.toggleTodo(id)
-      const startState = initialState.set('todos', currentTodos)
-      const newState = reducer(startState, action)
-      assert.deepEqual(newState.todos, expectedTodos)
-    })
-
-    invalidInputs.forEach(testCase => {
-      const { id } = testCase
-      const action = actionCreators.toggleTodo(id)
-      const currentTodos = [
-        { id: 0, title: 'foo', description: 'bar', completed: false }
+  const tests = [
+    {
+      name: 'ADD_TODO adds the first todo',
+      state: constructState([]),
+      action: actionCreators.addTodo(aTodo.title, aTodo.description),
+      expectedTodos: [{ ...aTodo, completed: false, id: 0 }]
+    },
+    {
+      name: 'ADD_TODO adds a todo with no description',
+      state: constructState([]),
+      action: actionCreators.addTodo(aTodo.title),
+      expectedTodos: [
+        { title: aTodo.title, description: undefined, completed: false, id: 0 }
       ]
-      const startState = initialState.set('todos', currentTodos)
-      const newState = reducer(startState, action)
-      assert.deepEqual(newState.todos, startState.todos)
+    },
+    {
+      name: 'ADD_TODO adds a todo when there is already a todo',
+      state: constructState([{ ...aCompletedTodo, completed: false }]),
+      action: actionCreators.addTodo('meow', 'meow'),
+      expectedTodos: [
+        { ...aCompletedTodo, completed: false },
+        { title: 'meow', description: 'meow', id: 1, completed: false }
+      ]
+    },
+    {
+      name: "ADD_TODO doesn't add a todo when it's bunk",
+      state: constructState([]),
+      action: actionCreators.addTodo(),
+      expectedTodos: []
+    },
+    {
+      name: 'EDIT_TODO edits a todo',
+      state: constructState([
+        { id: 0, title: 'cat', description: 'woof', completed: false }
+      ]),
+      action: actionCreators.editTodo(0, aTodo.title, aTodo.description, true),
+      expectedTodos: [aCompletedTodo]
+    },
+    {
+      name: 'EDIT_TODO does nothing when there are no todos to edit',
+      state: constructState([]),
+      action: actionCreators.editTodo(0, aTodo.title, aTodo.description, true),
+      expectedTodos: []
+    },
+    {
+      name: 'EDIT_TODO does nothing when todo does not exist',
+      state: constructState([aCompletedTodo]),
+      action: actionCreators.editTodo(1, aTodo.title, aTodo.description, true),
+      expectedTodos: [aCompletedTodo]
+    },
+    {
+      name: 'TOGGLE_TODO toggles an incomplete todo',
+      state: constructState([{ ...aCompletedTodo, completed: false }]),
+      action: actionCreators.toggleTodo(aCompletedTodo.id),
+      expectedTodos: [aCompletedTodo]
+    },
+    {
+      name: 'TOGGLE_TODO toggles a completed todo',
+      state: constructState([aCompletedTodo]),
+      action: actionCreators.toggleTodo(aCompletedTodo.id),
+      expectedTodos: [{ ...aCompletedTodo, completed: false }]
+    },
+    {
+      name: 'TOGGLE_TODO does nothing when no ID is given',
+      state: constructState([aCompletedTodo]),
+      action: actionCreators.toggleTodo(),
+      expectedTodos: [aCompletedTodo]
+    },
+    {
+      name: 'TOGGLE_TODO does nothing when undefined is given',
+      state: constructState([aCompletedTodo]),
+      action: actionCreators.toggleTodo(undefined),
+      expectedTodos: [aCompletedTodo]
+    },
+    {
+      name: 'TOGGLE_TODO does nothing when foo is given',
+      state: constructState([aCompletedTodo]),
+      action: actionCreators.toggleTodo('foo'),
+      expectedTodos: [aCompletedTodo]
+    },
+    {
+      name: 'TOGGLE_TODO does nothing when NaN is given',
+      state: constructState([aCompletedTodo]),
+      action: actionCreators.toggleTodo(Number.NaN),
+      expectedTodos: [aCompletedTodo]
+    },
+    {
+      name: 'TOGGLE_TODO does nothing when -infinity is given',
+      state: constructState([aCompletedTodo]),
+      action: actionCreators.toggleTodo(Number.NEGATIVE_INFINITY),
+      expectedTodos: [aCompletedTodo]
+    }
+  ]
+
+  tests.forEach(test => {
+    it(test.name, () => {
+      const actualState = reducer(test.state, test.action)
+      assert.deepEqual(actualState.todos, test.expectedTodos)
     })
   })
 })

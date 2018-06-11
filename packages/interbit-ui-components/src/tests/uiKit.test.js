5981171c078edb7989e85b981e5c575a723399f9
Karin Arikushi <karin@btl.co>
2018-06-11 10:53:54 -0700

    test(interbit-ui-components): write tests for UI Kit components

--- packages/interbit-ui-components/src/tests/uiKit.test.js
+++ packages/interbit-ui-components/src/tests/uiKit.test.js
@@ -8,8 +8,11 @@ import CallToAction from '../components/UIKit/CallToAction'
 import Card from '../components/UIKit/Card'
 import ConnectingTo from '../components/UIKit/ConnectingTo'
 import ContentBar from '../components/UIKit/ContentBar'
+import ContentBarDefault from '../components/UIKit/ContentBarDefault'
+import ContentBox from '../components/UIKit/ContentBox'
 import IconButton from '../components/UIKit/IconButton'
 import LinkWrapper from '../components/UIKit/LinkWrapper'
+import Markdown from '../components/Markdown'
 import placeholder from '../assets/placeholder.svg'
 
 configure({ adapter: new Adapter() })
@@ -20,7 +23,7 @@ const countChildren = (parent, child, count) => {
 }
 
 describe('<ButtonLink />', () => {
-  it('renders one <IconButton />', () => {
+  it('renders an <IconButton />', () => {
     countChildren(<ButtonLink text="foo" />, IconButton, 1)
   })
 })
@@ -66,6 +69,7 @@ describe('<ConnectingTo />', () => {
 
   it('renders an image if there is one', () => {
     countChildren(<ConnectingTo image={placeholder} {...props} />, 'img', 1)
+    countChildren(<ConnectingTo {...props} />, 'img', 0)
   })
 
   it('renders a spinner if there is one', () => {
@@ -74,6 +78,7 @@ describe('<ConnectingTo />', () => {
       'div.spinner',
       1
     )
+    countChildren(<ConnectingTo {...props} />, 'div.spinner', 0)
   })
 })
 
@@ -92,4 +97,90 @@ describe('<ContentBar />', () => {
     countChildren(<ContentBar {...props} />, 'h3', 1)
     countChildren(<ContentBar {...props} />, 'a', 0)
   })
+})
+
+describe('<ContentBarDefault />', () => {
+  const props = {}
+  beforeEach(() => {
+    props.title = 'foo'
+    props.content = 'bar'
+    props.image = placeholder
+  })
+
+  it('renders a <ContentBar />', () => {
+    countChildren(<ContentBarDefault {...props} />, ContentBar, 1)
+  })
+
+  it('renders the status if there is one', () => {
+    const status = {
+      type: 'done',
+      text: 'status is done'
+    }
+    countChildren(
+      <ContentBarDefault {...props} status={status} />,
+      `.${status.type}`,
+      1
+    )
+  })
+
+  it('renders an <a /> if there is a CTA', () => {
+    const callToAction = {
+      type: 'button',
+      text: 'foo',
+      to: '#'
+    }
+    countChildren(
+      <ContentBarDefault {...props} callToAction={callToAction} />,
+      'a',
+      1
+    )
+    countChildren(<ContentBarDefault {...props} />, 'a', 0)
+  })
+})
+
+describe('<ContentBox />', () => {
+  const props = {}
+  beforeEach(() => {
+    props.title = 'foo'
+    props.content = 'bar'
+  })
+
+  it('renders an <h2 />', () => {
+    countChildren(<ContentBox {...props} />, 'h2', 1)
+  })
+
+  it('renders a <Markdown />', () => {
+    countChildren(<ContentBox {...props} />, Markdown, 1)
+  })
+
+  it('renders a <CallToAction /> if there is one', () => {
+    const callToAction = {
+      text: 'foo'
+    }
+    countChildren(
+      <ContentBox callToAction={callToAction} {...props} />,
+      CallToAction,
+      1
+    )
+    countChildren(<ContentBox {...props} />, CallToAction, 0)
+  })
+})
+
+describe('<IconButton />', () => {
+  const props = {}
+  beforeEach(() => {
+    props.text = 'foo'
+  })
+
+  it('renders an <img /> if there is one', () => {
+    countChildren(<IconButton image={placeholder} {...props} />, 'img', 1)
+    countChildren(<IconButton {...props} />, 'img', 0)
+  })
+
+  it('renders an <i/> if there is an icon', () => {
+    countChildren(<IconButton icon={placeholder} {...props} />, 'i', 1)
+    countChildren(<IconButton {...props} />, 'i', 0)
+  })
+
+  it('passes href prop to <Button /> only if clickHandler is undefined', () => {})
 })

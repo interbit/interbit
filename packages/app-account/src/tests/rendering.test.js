8b8e97b3ea9db974f34b2112fb5e7fcad29b0db9
nicholeous <nicole.a.demers@gmail.com>
2018-07-20 14:10:59 -0700

    Render tests for all containers in app-account

--- packages/app-account/src/tests/rendering.test.js
+++ packages/app-account/src/tests/rendering.test.js
@@ -8,8 +8,12 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import App from '../App'
 import NotFoundPage from '../containers/NotFoundPage'
 import Home from '../containers/Home'
+import PageContainer from '../containers/PageContainer'
 import { Account } from '../containers/Account'
 import { ChainConnect } from '../containers/ChainConnect'
+import { EmailSuccess } from '../containers/EmailSuccess'
+import { CreateAccount } from '../containers/CreateAccount'
+import { InteractiveChains } from '../containers/InteractiveChains'
 import reducers from '../redux'
 
 const renderWithContext = component => {
@@ -67,8 +71,60 @@ describe('Renders without crashing:', () => {
       renderWithContext(<ChainConnect />)
     })
 
+    it('ChainConnect', () => {
+      renderWithContext(<ChainConnect mode={0} />)
+    })
+
+    it('ChainConnect', () => {
+      renderWithContext(<ChainConnect mode={1} />)
+    })
+
     it('Home', () => {
       renderWithContext(<Home />)
+    })
+
+    it('Home', () => {
+      renderWithContext(<Home isLoggedIn />)
+    })
+
+    it('EmailSuccess', () => {
+      renderWithContext(<EmailSuccess />)
+    })
+
+    it('CreateAccount', () => {
+      const props = {
+        content: {
+          signIn: {}
+        },
+        contentBars: {
+          gitHubCreateAccount: {},
+          gitHubSignIn: {},
+          attention: {
+            callToAction: {}
+          }
+        },
+        modals: {}
+      }
+      renderWithContext(<CreateAccount {...props} />)
+    })
+
+    it('PageContainer', () => {
+      renderWithContext(<PageContainer />)
+    })
+
+    it('InteractiveChains', () => {
+      const props = {
+        selectedChain: {},
+        blockchainDispatch: () => {}
+      }
+      renderWithContext(<InteractiveChains {...props} />)
+    })
+
+    it('InteractiveChains', () => {
+      const props = {
+        blockchainDispatch: () => {}
+      }
+      renderWithContext(<InteractiveChains {...props} />)
     })
   })
 })

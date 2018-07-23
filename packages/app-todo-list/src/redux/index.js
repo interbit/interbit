a25a2761bd5b654490237f950e3eef1b3e2fcc6e
David Mills <30841719+djlmills@users.noreply.github.com>
2018-07-23 16:12:34 -0700

    Fix app-todo-list dependencies

--- packages/app-todo-list/src/redux/index.js
+++ packages/app-todo-list/src/redux/index.js
@@ -1,12 +1,11 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
-import { reducer as interbitReducer } from 'interbit-ui-tools'
-import exploreChainReducer from './exploreChainReducer'
+import { interbitRedux, blockExplorerRedux } from 'interbit-ui-tools'
 import { reducer as uiReducer } from './uiReducer'
 
 export default combineReducers({
   form: formReducer,
-  interbit: interbitReducer,
-  exploreChain: exploreChainReducer,
+  interbit: interbitRedux.reducer,
+  blockExplorer: blockExplorerRedux.reducer,
   ui: uiReducer
 })

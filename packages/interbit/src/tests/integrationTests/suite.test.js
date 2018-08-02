c7bb4d883c6fbbecffd68f6fdcf9e48e3dd6a6f3
nicholeous <nicole.a.demers@gmail.com>
2018-08-02 12:37:52 -0700

    Timeout on interbit pkg tests++; remove redundant interbit-integration repo

--- packages/interbit/src/tests/integrationTests/suite.test.js
+++ packages/interbit/src/tests/integrationTests/suite.test.js
@@ -3,7 +3,13 @@ const testDeploy = require('./deploy')
 const testBuild = require('./build')
 const testStart = require('./start')
 
-test('interbit build from config to specified directory', testBuild, 10000)
-test('interbit start in dev mode', testStart, 10000)
-test('interbit keys generates a keypair', testKeys, 5000)
-test('interbit keys>build>deploy runs correctly configured', testDeploy, 20000)
+const TIMEOUT = 30000
+
+test('interbit build from config to specified directory', testBuild, TIMEOUT)
+test('interbit start in dev mode', testStart, TIMEOUT)
+test('interbit keys generates a keypair', testKeys, TIMEOUT)
+test(
+  'interbit keys>build>deploy runs correctly configured',
+  testDeploy,
+  TIMEOUT
+)

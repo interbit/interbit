49abce56316a6fac440cb93ad45da3bf2c44e379
David Mills <30841719+djlmills@users.noreply.github.com>
2018-08-10 16:26:47 -0700

    Try using done() in beforeAll, afterAll

--- packages/interbit-test/test/interbit.test.js
+++ packages/interbit-test/test/interbit.test.js
@@ -55,22 +55,25 @@ describe('interbit', () => {
     let keyPair
     const env = Object.assign({}, process.env)
 
-    beforeAll(async () => {
+    beforeAll(async done => {
       console.log('Generating key pair...')
       keyPair = await interbit.generateKeyPair()
 
       console.log('Creating hypervisor...')
       process.env.DB_PATH = `./db-${Date.now()}`
       hypervisor = await interbit.createHypervisor({ keyPair })
+
+      done()
     }, CI_INTERBIT_KEY_GEN_TIMEOUT)
 
-    afterAll(async () => {
+    afterAll(async done => {
       if (hypervisor) {
         console.log('Stopping hyperblocker...')
         hypervisor.stopHyperBlocker()
         hypervisor = undefined
       }
       process.env = env
+      done()
     })
 
     it('has expected API', () => {

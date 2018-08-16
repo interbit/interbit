e5ddf228e126219c31fa28f8eca4e69e70efd88c
Karin Arikushi <karin@btl.co>
2018-08-16 14:31:46 -0700

    (app-account): build grid scaffolding for profile edit section

--- packages/app-account/src/containers/Profile.js
+++ packages/app-account/src/containers/Profile.js
@@ -4,7 +4,13 @@ export default class Profile extends Component {
   render() {
     return (
       <div className="ibweb-page">
-        <div className="grid-cont two-col">profile page stub</div>
+        <div className="grid-cont two-col">
+          <div className="name">Name</div>
+          <div className="edit">edit</div>
+          <div className="line">line</div>
+          <div className="col1">col 1</div>
+          <div className="col2">col 2</div>
+        </div>
       </div>
     )
   }

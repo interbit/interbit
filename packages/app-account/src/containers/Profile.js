5454e5aaf284abfa97f3031dc01e37eda0154325
Karin Arikushi <karin@btl.co>
2018-08-16 12:51:19 -0700

    (app-account): create Profile container stub for new profile page

---
+++ packages/app-account/src/containers/Profile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Profile extends Component {
+  render() {
+    return (
+      <div className="ibweb-page">
+        <div className="grid-cont two-col">profile page stub</div>
+      </div>
+    )
+  }
+}

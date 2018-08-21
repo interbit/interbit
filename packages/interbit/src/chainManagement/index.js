cfc16eecde03f2dd5d512f8811112e0963c80eb3
nicholeous <nicole.a.demers@gmail.com>
2018-08-21 13:29:51 -0700

    Add forgotten function export

--- packages/interbit/src/chainManagement/index.js
+++ packages/interbit/src/chainManagement/index.js
@@ -1,5 +1,6 @@
 const configureChains = require('./configureChains')
 const createChains = require('./createChains')
+const destroyRemovedChains = require('./destroyRemovedChains')
 const generateDeploymentDetails = require('./generateDeploymentDetails')
 const initializeCovenants = require('./initializeCovenants')
 const startInterbit = require('./startInterbit')
@@ -9,6 +10,7 @@ const watchChain = require('./watchChain')
 module.exports = {
   configureChains,
   createChains,
+  destroyRemovedChains,
   generateDeploymentDetails,
   initializeCovenants,
   startInterbit,

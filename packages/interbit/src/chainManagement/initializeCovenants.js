8641cb83ab6255c2108276dd9346c76c5b61e9f5
nicholeous <nicole.a.demers@gmail.com>
2018-08-28 12:57:37 -0700

    Use new logger in new code (shiny!)

--- packages/interbit/src/chainManagement/initializeCovenants.js
+++ packages/interbit/src/chainManagement/initializeCovenants.js
@@ -6,6 +6,7 @@ const {
     selectors: { covenantHash: getCovenantHash }
   }
 } = require('interbit-covenant-tools')
+const log = require('../log')
 
 /**
  * Checks each of the configured static chains loaded on this cli for an
@@ -15,21 +16,21 @@ const {
  * @param {Object} manifest - The manifest configuration applied to this node
  */
 const initializeCovenants = async (cli, manifest, options) => {
-  console.log('INITIALIZING COVENANTS')
+  log.info('INITIALIZING COVENANTS')
   const chainEntries = Object.entries(getChains(manifest))
   const covenantPromises = []
   for (const [chainAlias, chainId] of chainEntries) {
     const chainInterface = cli.getChain(chainId)
     const state = chainInterface.getState()
-    console.log(`STATE FOR ${chainAlias}:`)
-    console.log(JSON.stringify(state, null, 2))
+    log.info(`STATE FOR ${chainAlias}:`)
+    log.info(JSON.stringify(state, null, 2))
     const currentCovenantHash = getCovenantHash(state)
     if (!currentCovenantHash) {
       const configuredCovenantHash = getCovenantHashByAlias(
         chainAlias,
         manifest
       )
-      console.log(
+      log.info(
         `No covenant found on ${chainAlias}... applying covenant ${configuredCovenantHash}`
       )
       const covenantPromise = cli.applyCovenant(chainId, configuredCovenantHash)

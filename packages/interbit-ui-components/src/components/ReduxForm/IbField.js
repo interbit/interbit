7d94e672868fdaef957525ca21fef1885d20cca0
Karin Arikushi <karin@btl.co>
2018-09-11 16:12:02 -0700

    (interbit-ui-components): refactor IbField renderInput function, add test skeleton for reduxForm

--- packages/interbit-ui-components/src/components/ReduxForm/IbField.js
+++ packages/interbit-ui-components/src/components/ReduxForm/IbField.js
@@ -1,23 +1,43 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { FormControl, Checkbox } from 'react-bootstrap'
+import { FormControl, Checkbox, Radio } from 'react-bootstrap'
 import { Field } from 'redux-form'
 
 // eslint-disable-next-line
-export const renderInputNew = ({onChange, props, label, placeholder, type, input, meta: {touched, error, warning}}) => (
-  <div className={touched && error ? 'field-error' : ''}>
-    {type === 'checkbox' ? (
-      <Checkbox placeholder={placeholder} {...input}>
-        {label}
-      </Checkbox>
-    ) : (
-      <FormControl placeholder={placeholder} type={type} {...input} />
-    )}
-    {touched &&
-      ((error && <span className="error-msg">{error}</span>) ||
-        (warning && <span>{warning}</span>))}
-  </div>
-)
+export const renderInputNew = ({onChange, props, label, placeholder, type, input, meta: {touched, error, warning}}) => {
+  let formControl
+
+  switch (type) {
+    case 'checkbox':
+      formControl = (
+        <Checkbox placeholder={placeholder} {...input}>
+          {label}
+        </Checkbox>
+      )
+      break
+    case 'radio':
+      formControl = (
+        <Radio placeholder={placeholder} {...input}>
+          {label}
+        </Radio>
+      )
+      break
+    default:
+      formControl = (
+        <FormControl placeholder={placeholder} type={type} {...input} />
+      )
+      break
+  }
+
+  return (
+    <div className={touched && error ? 'field-error' : ''}>
+      {formControl}
+      {touched &&
+        ((error && <span className="error-msg">{error}</span>) ||
+          (warning && <span>{warning}</span>))}
+    </div>
+  )
+}
 
 export default class IbField extends Component {
   static propTypes = {

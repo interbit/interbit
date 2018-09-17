1cb67a75040f645067cda1cb8417076790b7dd8a
Karin Arikushi <karin@btl.co>
2018-09-17 14:03:14 -0700

    (interbit-ui-components): update styleguide config so that styleguide:build runs

--- packages/interbit-ui-components/styleguide.config.js
+++ packages/interbit-ui-components/styleguide.config.js
@@ -2,8 +2,8 @@ const path = require('path')
 
 module.exports = {
   components: 'src/components/**/[A-Z]*.js',
-  require: [path.join(__dirname, '/src/css/interbit.css')],
-  assetsDir: 'src/assets',
+  require: [path.join(__dirname, '/dist/css/interbit.css')],
+  assetsDir: path.join(__dirname, '/src/assets'),
   template: 'styleguide/template.html',
   ignore: ['**/src/components/Welcome.js']
 }

3000f5a3d6c6891a01f040d0438ebfa12cb4fd4a
Karin Arikushi <karin@btl.co>
2018-10-18 12:34:42 -0700

    (interbit-ui-components): upgrade to react-styleguidist v.7.3.11 to fix styleguide:build errors

--- packages/interbit-ui-components/styleguide.config.js
+++ packages/interbit-ui-components/styleguide.config.js
@@ -4,7 +4,6 @@ module.exports = {
   components: 'src/components/**/[A-Z]*.js',
   require: [path.join(__dirname, '/dist/css/interbit.css')],
   assetsDir: path.join(__dirname, '/src/assets'),
-  template: 'styleguide/template.html',
   ignore: [
     '**/src/components/Welcome.js',
     '**/src/components/BlockExplorer/**',
